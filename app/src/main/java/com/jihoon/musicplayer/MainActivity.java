07f792f137227a34a3a1e881c6857dc218090ff6
Kim Ji Hoon <kimjihun1001@gmail.com>
2022-01-11 17:18:08 +0900

    추가: 새로운 플레이리스트 생성

--- app/src/main/java/com/jihoon/musicplayer/MainActivity.java
+++ app/src/main/java/com/jihoon/musicplayer/MainActivity.java
@@ -19,6 +19,8 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static ArrayList<Model_playlist> List_Model_playlist = new ArrayList<Model_playlist>();
+
     LinearLayout container_playlist;
     Context nowContext;
 
@@ -29,9 +31,15 @@ public class MainActivity extends AppCompatActivity {
 
         container_playlist = findViewById(R.id.container_playlist);
         nowContext = container_playlist.getContext();
-    }
 
-    public static ArrayList<Model_playlist> List_Model_playlist = new ArrayList<Model_playlist>();
+        for(Model_playlist model_playlist: List_Model_playlist) {
+            LinearLayout linearLayout = new LinearLayout(nowContext);
+            TextView textView = new TextView(nowContext);
+            textView.setText(model_playlist.name);
+            linearLayout.addView(textView);
+            container_playlist.addView(linearLayout);
+        }
+    }
 
     public void Click_btn_newPlaylist(View view) {
 
@@ -44,38 +52,32 @@ public class MainActivity extends AppCompatActivity {
         final EditText input = new EditText(this);
         alert_newPlaylist.setView(input);
 
-        //
+        // 확인 누를 경우
         alert_newPlaylist.setPositiveButton("확인", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 String value = input.getText().toString();
-                // value.toString();
 
                 Model_playlist newPlaylist = new Model_playlist();
                 newPlaylist.name = value;
                 List_Model_playlist.add(newPlaylist);
 
-                for(Model_playlist model_playlist: List_Model_playlist) {
-                    LinearLayout linearLayout = new LinearLayout(nowContext);
-                    TextView textView = new TextView(nowContext);
-                    textView.setText(model_playlist.name);
-                    linearLayout.addView(textView);
-                    container_playlist.addView(linearLayout);
-                }
+                LinearLayout linearLayout = new LinearLayout(nowContext);
+                TextView textView = new TextView(nowContext);
+                textView.setText(newPlaylist.name);
+                linearLayout.addView(textView);
+                container_playlist.addView(linearLayout);
 
             }
         });
 
+        // 취소 누를 경우
         alert_newPlaylist.setNegativeButton("취소", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
                 // 취소됨
             }
         });
-
-
-        // AlertDialog alertDialog = builder.create();
-        //alertDialog.show();
 
         alert_newPlaylist.show();
     }
